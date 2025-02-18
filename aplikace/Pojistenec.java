d62958cc2384c575ff1f4a21b8a3994f2f962796
User-Alex-26 <poutnikalex2004@gmail.com>
2025-02-18 23:28:31 +0100

    Create Pojistenec.java

---
+++ aplikace/Pojistenec.java
@@ -0,0 +1,28 @@
+package aplikace;
+
+public class Pojistenec {
+    private String jmeno;
+    private String prijmeni;
+    private int vek;
+    private String telefon;
+
+    public Pojistenec(String jmeno, String prijmeni, int vek, String telefon) {
+        this.jmeno = jmeno;
+        this.prijmeni = prijmeni;
+        this.vek = vek;
+        this.telefon = telefon;
+    }
+
+    public String getJmeno() {
+        return jmeno;
+    }
+
+    public String getPrijmeni() {
+        return prijmeni;
+    }
+
+    @Override
+    public String toString() {
+        return jmeno + "   " + prijmeni + "   " + vek + "   " + telefon;
+    }
+}
